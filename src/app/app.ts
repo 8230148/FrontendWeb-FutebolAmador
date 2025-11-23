131c76291a5b35e67ac31208455da24a8fcc739d
8230148 <8230148@estg.ipp.pt>
2025-11-23 00:33:20 +0000

    ajustes paginas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/partials/header/header';
 import { TeamService } from './services/team.service';
 import { Footer } from './components/partials/footer/footer';
 
 @Component({
   selector: 'app-root',
+<<<<<<< Updated upstream
   imports: [RouterOutlet, Header, Footer],
+=======
+  imports: [RouterOutlet],
+>>>>>>> Stashed changes
   templateUrl: './app.html',
   styleUrl: './app.css',
+  standalone: true
 })
 export class App {
   protected readonly title = signal('frontend');
